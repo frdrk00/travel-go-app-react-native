164f6b84bb54b21a76c301eca7a5256f4973412f
frdrk00 <masterkooo@gmail.com>
2023-05-23 23:27:41 +0200

    Discover - Header

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Discover" component={Discover} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/Discover.js
@@ -0,0 +1,35 @@
+import { View, Text, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Avatar } from "../assets";
+
+const Discover = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="flex-1 bg-white relative">
+      <View className="flex-row items-center justify-between px-8">
+        <View>
+          <Text className="text-[40px] text-[#0B646B] font-bold">Discover</Text>
+          <Text className="text-[#527283] text-[36px]">the Beauty today</Text>
+        </View>
+
+        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center shadow-lg">
+          <Image
+            source={Avatar}
+            className="w-full h-full rounded-md object-cover"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Discover;
