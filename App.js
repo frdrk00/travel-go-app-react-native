93dbbd51bdb3af071d0b8d0021e8e9954fdc8389
frdrk00 <masterkooo@gmail.com>
2023-05-23 23:18:51 +0200

    HomeScreen complete

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import Discover from "./screens/Discover";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
@@ -11,6 +12,5 @@ export default function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
-
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,69 @@
-import {  Text } from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View, Image, TouchableOpacity } from "react-native";
 import React, { useLayoutEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as Animatable from "react-native-animatable";
 import { useNavigation } from "@react-navigation/native";
+import { HeroImage } from "../assets";
 
 const HomeScreen = () => {
-    const navigation = useNavigation()
+  const navigation = useNavigation();
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false
-        })
-    }, [])
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <SafeAreaView>
-      <Text className=''>HomeScreen</Text>
+    <SafeAreaView className="bg-white flex-1 relative">
+      {/* First Section */}
+      <View className="flex-row px-6 mt-8 items-center space-x-2">
+        <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+          <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
+        </View>
+
+        <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+      </View>
+
+      {/* Second Section */}
+      <View className="px-6 mt-8 space-y-3">
+        <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
+        <Text className="text-[#00BCC9] text-[38px] font-bold">
+          Good Moments
+        </Text>
+
+        <Text className="text-[#3C6072] text-base">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+          facere, tempora excepturi aliquam iure.
+        </Text>
+      </View>
+
+      {/* Circle Section */}
+      <View className="w-[400px] h-[400px] bg-[#00BCC9] rounded-full absolute bottom-36 -right-36" />
+      <View className="w-[400px] h-[400px] bg-[#E99265] rounded-full absolute -bottom-28 -left-36" />
+
+      {/* Image container */}
+      <View className="flex-1 relative items-center justify-center">
+        <Animatable.Image
+          animation="fadeIn"
+          easing="ease-in-out"
+          source={HeroImage}
+          className="w-full h-full object-cover mt-20"
+        />
+
+        <TouchableOpacity 
+        onPress={() => navigation.navigate('Discover')}
+        className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 border-[#00BCC9] rounded-full items-center justify-center">
+          <Animatable.View
+            animation="pulse"
+            easing="ease-in-out"
+            iterationCount="infinite"
+            className="w-20 h-20 items-center justify-center rounded-full bg-[#00BCC9]"
+          >
+            <Text className="text-gray-50 text-[36px] font-bold">Go</Text>
+          </Animatable.View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
